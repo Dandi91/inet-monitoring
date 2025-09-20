48dcd507b049c656908e20f8be639a039ff75c26
Aleksandr Andreev <dandi.vlg@gmail.com>
2025-09-21 01:13:38 +0200

    Optimize HTTP a bit

--- src/server.rs
+++ src/server.rs
@@ -7,18 +7,19 @@ use std::thread;
 pub fn serve(port: u16) -> thread::JoinHandle<()> {
     thread::spawn(move || {
         let encoder = prometheus::TextEncoder::new();
+        let headers = format!("HTTP/1.1 200 OK\r\nContent-Type: {}\r\n\r\n", encoder.format_type());
+
         let listener = TcpListener::bind(("0.0.0.0", port)).expect("unable to start HTTP server");
         println!("Listening to connections on port {}", port);
+
         loop {
             match listener.accept() {
                 Ok((mut stream, remote)) => {
                     let mut reader = BufReader::new(&stream);
-                    let mut request = String::new();
+                    let mut request = String::with_capacity(128);
                     reader.read_line(&mut request).unwrap_or_default();
 
-                    let headers = format!("HTTP/1.1 200 OK\r\nContent-Type: {}\r\n\r\n", encoder.format_type());
                     stream.write_all(headers.as_bytes()).unwrap_or_default();
-
                     let metrics = prometheus::gather();
                     encoder.encode(&metrics, &mut stream).unwrap_or_default();
 
