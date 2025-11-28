bc81b3dc474413bb87349d9b88942f4b969c08f6
Aleksandr Andreev <dandi.vlg@gmail.com>
2025-11-28 01:41:03 +0100

    Handle multiple termination signals

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,19 @@ mod server;
 mod speedtest;
 
 use crate::config::Config;
+use std::io;
 use std::time::Duration;
-use tokio::{signal, task};
+use tokio::signal::unix::{SignalKind, signal};
+use tokio::{select, task};
+
+async fn wait_for_signal() -> io::Result<()> {
+    let mut terminate = signal(SignalKind::terminate())?;
+    let mut interrupt = signal(SignalKind::interrupt())?;
+    select! {
+        _ = terminate.recv() => Ok(()),
+        _ = interrupt.recv() => Ok(()),
+    }
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,7 +33,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Duration::from_secs_f32(config.speedtest_timeout),
     ));
 
-    signal::ctrl_c().await?;
-    eprintln!("Received CTRL-C, shutting down...");
+    wait_for_signal().await?;
+    eprintln!("Received signal, shutting down...");
     Ok(())
 }
